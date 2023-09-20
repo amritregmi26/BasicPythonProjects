b7ea319596e23824ba91f31a54323c9fbfd113bc
= <=>
2023-09-20 12:50:09 +0545

    Required updation made in Todo App

--- Todo Desktop App/Todo.py
+++ Todo Desktop App/Todo.py
@@ -1,7 +1,9 @@
 import todoFile
 import PySimpleGUI as sg
+import time
 
-#Creating label and input box to accept todo
+sg.theme("Black")
+clock = sg.Text('',key='clock')
 label = sg.Text("Type in a work to do: ")
 input_box = sg.InputText(tooltip="Enter Todo", key="todoItem")
 add_button = sg.Button("Add")
@@ -14,14 +16,16 @@ complete_btn = sg.Button("Complete")
 exit_btn = sg.Button("Exit")
 
 window = sg.Window('ToDo APP',
-                   layout = [[label], 
+                   layout = [[clock],
+                             [label], 
                              [input_box], 
                              [add_button], 
                              [list_box,edit_btn, complete_btn],
                              [exit_btn]],
                    font = ("Roboto", 14))
 while True:
-    btn_label, values = window.read()
+    btn_label, values = window.read(timeout=900)
+    window['clock'].update(value = time.strftime("%b %d, %Y %H:%M:%S"))
     match btn_label:
         case "Add":
             todo = todoFile.get_todos()
@@ -33,27 +37,35 @@ while True:
             window["todoItem"].update(value="")
 
         case "Edit":
-            edit_todo = values["todo"][0]
-            new_todo = values['todoItem'] + "/n"
+            try:
+                edit_todo = values["todo"][0]
+                new_todo = values['todoItem'] + "/n"
 
-            todos = todoFile.get_todos()
-            index = todos.index(edit_todo)
-            todos[index] = new_todo
-            todoFile.write_todos(todos)
+                todos = todoFile.get_todos()
+                index = todos.index(edit_todo)
+                todos[index] = new_todo
+                todoFile.write_todos(todos)
 
-            window['todo'].update(values=todos)
+                window['todo'].update(values=todos)
+            
+            except IndexError:
+                sg.popup("Please select an item")
         
         case 'todo':
             window["todoItem"].update(value=values['todo'][0])
 
         case 'Complete':
-            completed_todo = values["todo"][0]
-            todos = todoFile.get_todos()
-            todos.remove(completed_todo)
-            todoFile.write_todos(todos)
-            window["todo"].update(values=todos)
-            window["todoItem"].update(value="")
-        
+            try:
+                completed_todo = values["todo"][0]
+                todos = todoFile.get_todos()
+                todos.remove(completed_todo)
+                todoFile.write_todos(todos)
+                window["todo"].update(values=todos)
+                window["todoItem"].update(value="")
+            
+            except IndexError:
+                sg.popup("Please select an item")
+            
         case 'Exit':
             break
 
